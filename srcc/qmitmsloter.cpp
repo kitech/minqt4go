b1a8e899259d83cff8caf98afdc86d3bd5226b5f
egitop <egitop@uload.xyz>
2025-10-15 23:38:37 +0800

    callback to V, clear cbdata field

--- srcc/qmitmsloter.cpp
+++ srcc/qmitmsloter.cpp
@@ -8,13 +8,11 @@ void QMitmSloter::dummy() {}
 // maybe useful, senderSignalIndex(>=qt4)
 // QSignalMapper , QSignalSpy
 
+// see qtrt.ConnCbdata
 typedef struct ConnCbdata {
-	void* sender;
 	void* fnptr;
-	void* cbptr;
-	int argc;
-	void* argtys; //int argtys[];
-	void* fatptr0;
+	void* sender;
+	void* fatptr0; // vstring
 	void* fatptr1;
 } ConnCbdata;
 
@@ -25,6 +23,12 @@ QMitmSloter::QMitmSloter(void* d_) : QObject() {
 #if QT_VERSION < 0x040000
 void QMitmSloter::metacallir(int _id, QUObject* _o) {
 	ConnCbdata* d = (ConnCbdata*)this->cbdata;
+	// std::cout<<__FUNCTION__<<_id<<_o<<std::endl;
+	printf("%s: %d, %p, d=%p\n", __FUNCTION__, _id, _o, this->cbdata);
+	
+	typedef void (*cbfnty)(void*, int, void*);
+	cbfnty fno = (cbfnty)d->fnptr;
+	fno(d, _id, _o);
 }
 #else
 void QMitmSloter::metacallir(QObject *_o, QMetaObject::Call _c, int _id, void **_a);
